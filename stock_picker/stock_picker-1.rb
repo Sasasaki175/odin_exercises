7a933e7bd46d3e9ca1a123d3e8d2cac5141a4a1a
Sasasaki175 <207643966+Sasasaki175@users.noreply.github.com>
2025-04-17 10:58:00 +0900

    picker/stock_picker-1.rb

---
+++ stock_picker/stock_picker-1.rb
@@ -0,0 +1,19 @@
+def stock_picker(stocks)
+
+  max_profit = 0
+  max_profit_combination = Array.new
+  
+  stocks.each_with_index do |stock_price, day|
+    unless stocks[day + 1] == nil then
+      if (stocks[day + 1..].max - stock_price > max_profit) then
+        max_profit = stocks[day + 1..].max - stock_price
+        max_profit_combination = [day, stocks.index(stocks[day + 1..].max)]
+      end
+    end
+  end
+
+  p max_profit_combination
+end
+
+stock_picker([17,3,6,9,15,8,6,1,10])
+stock_picker([18,5,3,9,2,17,15,15,20])
